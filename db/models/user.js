566313be6a7c6489d123eb7e174d99ac89c1b446
Cam Tangalakis-Lippert <cami.lippert@gmail.com>
2021-10-18 13:12:48 -0700

    whatever

---
+++ db/models/user.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+      unique: true
+    },
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING(50)
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING(50)
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true
+    },
+    hashedPassword: {
+      allowNull: false,
+      type: DataTypes.STRING.BINARY
+    }
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
