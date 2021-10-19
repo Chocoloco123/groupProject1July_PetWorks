f44aacb4eeac6f0b349ed053f1591bd11f90223c
gavinfitch <gavin.fitch@gmail.com>
2021-10-18 17:24:44 -0700

    add question form

--- auth.js
+++ auth.js
@@ -1,26 +1,26 @@
 const db = require('./db/models');
 
 const loginUser = (req, res, user) => {
-    req.session.auth = {
-        userId: user.id,
-    };
+  req.session.auth = {
+    userId: user.id,
+  };
 };
 
 const logoutUser = (req, res) => {
-    delete req.session.auth;
+  delete req.session.auth;
 };
 
 const requireAuth = (req, res, next) => {
-    if (!res.locals.authenticated) {
-        return res.redirect('/user/login');
-    }
-    return next();
+  if (!res.locals.authenticated) {
+    return res.redirect('/user/login');
+  }
+  return next();
 };
 
 const restoreUser = async (req, res, next) => {
   // Log the session object to the console
   // to assist with debugging.
-//   console.log(req.session);
+  //   console.log(req.session);
 
   if (req.session.auth) {
     const { userId } = req.session.auth;
@@ -44,8 +44,8 @@ const restoreUser = async (req, res, next) => {
 };
 
 module.exports = {
-    loginUser,
-    logoutUser,
-    requireAuth,
-    restoreUser,
+  loginUser,
+  logoutUser,
+  requireAuth,
+  restoreUser,
 };
--- routes/question.js
+++ routes/question.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt = require('bcryptjs');
+const { requireAuth } = require('../auth');
 const { check, validationResult } = require('express-validator');
 const db = require('../db/models');
 
 const { csrfProtection, asyncHandler } = require('./utils');
 
 router.get('/', csrfProtection, (req, res) => {
-    res.render('question-form', {csrfToken: req.csrfToken()})
+    res.render('question-form', { csrfToken: req.csrfToken() })
 })
 
-router.post('/', csrfProtection, asyncHandler(async(req, res)=> {
-    
-    res.render('/')
+router.post('/', csrfProtection, requireAuth, asyncHandler(async (req, res) => {
+    const { question, category, petType } = req.body
+    const { userId } = req.session.auth;
+    const newQuestion = await db.Question.create({
+        userId,
+        question,
+        category,
+        petType
+    });
+
+    res.redirect('/');
 }))
 
 
