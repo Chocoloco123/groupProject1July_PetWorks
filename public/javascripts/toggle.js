8a7e662d2aaa278f61c0744dfcc51fd4a73fe8ca
Cam Tangalakis-Lippert <cami.lippert@gmail.com>
2021-10-21 10:20:05 -0700

    toggle buttons

--- public/javascripts/toggle.js
+++ public/javascripts/toggle.js
@@ -1,11 +1,14 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const commentToggle = document.getElementsByClassName('target')
+    const commentToggle = document.getElementById('commentsFeed')
     const toggleButton = document.getElementById('toggleButton')
 
     toggleButton.addEventListener('click', (e) => {
         console.log(commentToggle, toggleButton)
         // e.preventDefault();
-        if (commentToggle.visibility === 'hidden') commentToggle.visibility = 'visible'
-        else commentToggle.visibility = 'hidden'
+        if (commentToggle.getAttribute('class') === 'targetShow') commentToggle.setAttribute('class', 'targetHide')
+        else commentToggle.setAttribute('class', 'targetShow')
     })
+
+
+    const filterButton = document
 })
